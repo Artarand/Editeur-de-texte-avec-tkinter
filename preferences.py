981f3e56772e5f8efe6e87769239eb142b0c33f6
Lounys <142540009+Lounys-Dev@users.noreply.github.com>
2024-04-21 12:17:59 +0200

    Transforming app to a Custom Tkinter App

---
+++ preferences.py
@@ -0,0 +1,20 @@
+from typing import Literal
+
+APP_NAME = "Text Editor"
+
+MAIN_FONT = "American Typer"
+TITLE_SIZE = 28
+
+MAIN_COLOR = "#3C5B6F"
+DARK_MAIN_COLOR = "#153448"
+
+CLASSIC_T_COLORS = (MAIN_COLOR, DARK_MAIN_COLOR)
+REVERSED_T_COLORS = (DARK_MAIN_COLOR, MAIN_FONT)
+
+
+PREFERED_SELECTION_SIZE_FONT: Literal["slider", "spinbox"] = "slider"
+FONT_SIZE_RANGE = (1,100)
+
+
+WHITE = "#F6F5F2"
+BLACK = "#222831"
